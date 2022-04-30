6623dc6b80d746d675f1acb89cec65d36ff78c2d
ohad <ohad@theora.com>
2022-04-30 09:42:09 +0300

    gitDiffs

--- src/Joins.class.php
+++ src/Joins.class.php
@@ -7,34 +7,29 @@ class Joins extends Mdemo {
 			array(
 				'title' => "Authors",
 				'sql' => "select * from authors order by last, first",
-				'exportFileName' => "Authors",
 			),
 			array(
 				'title' => "Books",
 				'sql' => "select * from books order by title",
-				'exportFileName' => "Books",
 			),
 			array(
 				'title' => "Join",
 				'sql' => "select a.*, b.* from authors a, books b where a.id = b.authorId order by a.first, a.last, b.title",
-				'exportFileName' => "BooksAndAuthors",
 			),
 			array(
 				'title' => "Foundation",
 				'sql' => "select a.first, a.last, b.title from authors a, books b where a.id = b.authorId and b.title like '%Foundation%' order by a.first, a.last, b.title",
-				'exportFileName' => "Foundation",
 			),
 			array(
 				'title' => "The Complete Cartesian Product",
 				'sql' => "select a.*, b.* from authors a, books b order by a.first, a.last, b.title",
-				'exportFileName' => "Cartesian",
 			),
 		);
 		foreach ( $queries as $query ) {
 			$this->Mview->msg($query['title'], true);
 			$this->Mview->msg($query['sql']);
 			$rows = $this->Mmodel->getRows($query['sql']);
-			$this->showRows($rows, true, $query['exportFileName']);
+			$this->Mview->showRows($rows);
 			$this->Mview->br(2);
 		}
 		$file = "Joins.class.php";
